2cad4dc81c72bd97015674f8eeb0a74f70e91e2d
Vinicius Murad <vinicius.murad@olist.com>
2022-04-10 18:38:41 -0300

    chore(container): add CI/CD settings

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1>Header!</h1>
       <hr />
       <MarketingApp />
     </div>
