f7dbf1182e4f5845be82d9f362060c73b1386377
Vinicius Murad <vinicius.murad@olist.com>
2022-04-11 02:20:20 -0300

    feat(container): add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+import { BrowserRouter } from "react-router-dom";
+
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
