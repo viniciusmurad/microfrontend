726ea55adb2f2a4e4024dcd68a9be3de7f0a6f5d
Vinicius Murad <vinicius.murad@olist.com>
2022-04-13 01:59:49 -0300

    feat: add multi navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDom from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // MOUNT FUNCTION TO START UP THE APP
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDom.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // IF WE ARE IN DEVELOPMENT AND IN ISOLATION,
@@ -12,10 +28,11 @@ const mount = (el) => {
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
 // WE ARE RUNNING THROUGH CONTAINER
 // AND WE SHOULD EXPORT MOUNT FUNCTION
+
 export { mount };
