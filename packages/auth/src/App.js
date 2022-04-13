a35581463e6636e56b14661e61258513b95b028a
Vinicius Murad <vinicius.murad@olist.com>
2022-04-13 02:05:40 -0300

    feat: add auth module

---
+++ packages/auth/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Switch, Route, Router } from "react-router-dom";
+
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "au",
+});
+
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch></Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
+};
---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDom from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+// MOUNT FUNCTION TO START UP THE APP
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDom.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// IF WE ARE IN DEVELOPMENT AND IN ISOLATION,
+// CALL MOUNT INMEDIATELY
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// WE ARE RUNNING THROUGH CONTAINER
+// AND WE SHOULD EXPORT MOUNT FUNCTION
+
+export { mount };
